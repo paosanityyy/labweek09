0f0bfd79a3cfd7a5c96b82ac8f265d6a592e26d5
paolocasison <paolocasison@gmail.com>
2023-11-07 21:55:24 -0500

    Lab Week 09

--- src/App.js
+++ src/App.js
@@ -6,19 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <h3>101384585</h3>
+        <h4>Christian Paolo Casison</h4>
+        <h5>George Brown College, Toronto</h5>
       </header>
     </div>
+    
   );
 }
 
